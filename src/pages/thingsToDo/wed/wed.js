f5bc37a8aa9aacbf24f85c4f98bd90424424910a
duongtuan113 <141711273+duongtuan113@users.noreply.github.com>
2024-04-21 02:20:33 +0700

    ”tuan”

---
+++ src/pages/thingsToDo/wed/wed.js
@@ -0,0 +1,94 @@
+import { useParams } from "react-router-dom";
+import './wed.css'
+function Wed() {
+    const { Wed } = useParams();
+    return (
+
+        <div className="SectionHeader_sectionHeader__60CR7">
+            <div className="SectionHeader_title__eE2Rf">
+                <div className="flex flex-col gap-16">
+                    <h5 className="top">Top 5 resort nổi tiếng ở Hạ Long</h5>
+                    <div className="Badge_default__edPsE  Badge_sm__JAs9n Badge_container__ehKAY ">
+                        <label className="xs">11/04/2024</label>
+                    </div>
+                    <img className=" w-40" src="./icon/heading-border.png" />
+                </div>
+                <div className="img-blog" >
+                    <img className="img-1" src="https://minio.fares.vn/mixivivu-dev/tour/blog/images/apbb39vj3e99sczz.webp"></img>
+                </div>
+                <div class="BlogDetail_output__fDBDB">
+                    <p className="object">1.FLC Grand Hạ Long(FLC Hạ Long Bay Golf Club & Luxury Resort)</p>
+                    <p className="object">-Địa chỉ: Đường Nguyễn Văn Cừ, phường Hồng Hải, Thành phố Hạ Long</p>
+                    <p className="object">-Giá phòng tham khảo: 3.300.000 – 7.600.000 VNĐ/đêm/villa</p>
+                    <p className="object">Flc Hạ Long, các tòa căn hộ khách sạn đẳng cấp 5 sao tọa lạc trên đỉnh đồi trung tâm thành phố. Đây là nơi đẹp nhất để chiêm ngưỡng trọn vẹn kỳ quan thiên nhiên Vịnh Hạ Long. Sở hữu những tiện ích đẳng cấp hàng đầu như sân golf 18 hố dạng links, trung tâm hội nghị quốc tế 1500 chỗ, bể bơi bốn mùa 1000m2 và Skybar view vịnh cao nhất thành phố.</p>
+                </div>
+                <div className="img-blog">
+                    <img className="img-1" src="https://minio.fares.vn/mixivivu-dev/tour/ship/blog/fhdizuujdd4e8xay.webp"></img>
+                </div>
+                <p className="object">FLC Grand Hotel Hạ Long là khách sạn 5 sao duy nhất tại Quảng Ninh sở hữu phòng tổng thống với tiêu chuẩn an ninh hàng đầu, sảnh tiếp đón biệt lập, đầy đủ tiện nghi và dịch vụ.</p>
+                <div className="img-blog">
+                    <img className="img-1" src="https://minio.fares.vn/mixivivu-dev/tour/ship/blog/iekhosbgtd10zfu9.webp"></img>
+                </div>
+                <p className="object">Nhà hàng Bamboo với không gian đậm chất Trung Hoa thanh lịch là nơi thực khách khám phá tinh hoa ẩm thực Hồng Kông truyền thống với đa dạng những món ăn chuẩn vị. Bên cạnh đó, quý khách có thể nâng tầm trải nghiệm với các phòng riêng có nhân viên phục vụ hay tìm kiếm sự mới lạ trong những set menu phong cách Việt Nam.</p>
+                <p className="object">Paradise Sky Bar là nơi quý khách tìm thấy những loại rượu mạnh nổi tiếng từ khắp nơi trên thế giới hay biến tấu độc đáo</p>
+                <div className="img-blog">
+                    <img className="img-1" src="https://minio.fares.vn/mixivivu-dev/tour/ship/blog/67dlicejvdnctwi9.webp"></img>
+                </div>
+                <p className="object">Bể bơi bốn mùa trong nhà mang đến không gian bơi lội luôn mát mẻ về mùa hè, ấm nóng về mùa đông. Bạn hoàn toàn yên tâm vì nước được bổ sung, thay mới hàng ngày và không sử dụng hóa chất gây hại làm trong nước nên có thể thỏa thích ngâm mình và bơi lội nhiều giờ liên tục mà không sợ bị hư hại da, tóc.</p>
+                <div className="img-blog">
+                    <img className="img-1" src="https://minio.fares.vn/mixivivu-dev/tour/ship/blog/vlb1s67mvmmuq6x7.webp"></img>
+                </div>
+                <p className="object">2.Vinpearl Ha Long</p>
+                <p className="object">-Địa chỉ: Đảo Rều, Thành phố Hạ Long, Quảng Ninh</p>
+                <p className="object">-Giá phòng tham khảo: từ 2.600.000 – 5.000.000Đ/người</p>
+                <p className="object">Lấy cảm hứng từ các tác phẩm nghệ thuật và kiến ​​trúc Pháp, Vinpearl Hạ Long Resort mang đến cho du khách nơi nghỉ dưỡng sang trọng với thiết kế hình cánh cung trang nhã. Nhìn từ xa, khu nghỉ dưỡng như một vương quốc thu nhỏ, nguy nga và xinh đẹp sừng sững giữa trung tâm Vịnh Hạ Long – kỳ quan thiên nhiên thế giới.</p>
+                <p className="object">+</p>
+                <p className="object">Với diện tích lên đến 47 000m2, khu nghỉ dưỡng có đến 384 phòng nghỉ với nhiều hạng phòng khác nhau, phù hợp cho nhiều nhu cầu và nhiều đối tượng.</p>
+                <p className="object">+</p>
+                <p className="object">Vinpearl Hạ Long không chỉ phù hợp để nghỉ dưỡng, nơi đây còn là địa điểm lý tưởng để bạn có thể trải nghiệm những tiện ích dịch vụ cao cấp, chuẩn 5 sao quốc tế.</p>
+                <p className="object">+</p>
+                <p className="object">3.Premier Village Ha Long Bay Resort</p>
+                <p className="object">-Địa chỉ: số 9 đường Hạ Long, Thành phố Hạ Long</p>
+                <p className="object">- Giá phòng tham khảo: từ 3.200.000 – 15.300.000 VNĐ/đêm/villa</p>
+                <p className="object">Với không gian và thiết kế đón gió biển trong lành cùng với những tiện ích như hồ bơi riêng, biệt thự nghỉ dưỡng sang trọng sẽ đem đến cho bạn và gia đình cảm giác được hòa mình vào sự tươi mát của cỏ cây, hoa lá và đắm mình thư giãn trong tiếng sóng biển rì rào.</p>
+                <p className="object">+</p>
+                <p className="object">Những căn biệt thự sang trọng của Premier Village Ha Long Bay được thiết kế tràn đầy cảm hứng để hòa nhịp trọn vẹn với vẻ đẹp nhiệt đới bên bờ vịnh</p>
+                <p className="object">+</p>
+                <p className="object">Trải nghiệm những màn trình diễn ẩm thực sống động và tương tác trực tiếp với các đầu bếp hàng đầu</p>
+                <p className="object">+</p>
+                <p className="object">Cung cấp phòng các phòng massage trị liệu đôi và đơn. Đặc quyền sử dụng bể bơi và spa với trang thiết bị tiện nghi.</p>
+                <p className="object">+</p>
+                <p className="object">4.Royal Lotus Ha Long Resort & Villas</p>
+                <p className="object">-Địa chỉ: Bán Đảo 2, Đường Hùng Thắng, Thành phố Hạ Long</p>
+                <p className="object">-Giá phòng tham khảo: từ 1.500.000đ – 2.000.000đ</p>
+                <p className="object">Phong cách thiết kế của Resort Royal Lotus Hạ Long được lấy cảm hứng từ xu hướng sống xanh toàn cầu. Công trình là sự kết hợp độc đáo và hoàn hảo giữa vẻ đẹp hiện đại của những khu biệt thự, phòng nghỉ và sự trong lành, tươi mát của phong cảnh thiên nhiên xung quanh.</p>
+                <p className="object">+</p>
+                <p className="object">Royal Villas Hạ Long là khu nghỉ dưỡng duy nhất sở hữu hệ thống phòng khách sạn vừa có phòng nghỉ dưỡng dành cho gia đình. Chính vì vậy resort luôn là sự lựa chọn hàng đầu dành cho những chuyến du lịch nghỉ dưỡng của các đại gia đình hoặc những nhóm du khách đông người.</p>
+                <p className="object">+</p>
+                <p className="object">Không chỉ dừng lại ở đó, nằm trên mảnh đất nhô ra sát bờ biển nên Royal Lotus Hạ Long sở hữu 3 mặt tiền giáp biển. Chính ưu điểm này đã giúp đem tới cảm giác thoáng đãng, bình yên nhưng cũng vô cùng riêng tư cho du khách khi đặt chân tới mảnh đất này.</p>
+                <p className="object">+</p>
+                <p className="object">Với dịch vụ chuyên nghiệp cùng khung cảnh thiên nhiên hữu tình, nên thơ của Hạ Long, Lotus Hạ Long đang là resort được đánh giá cao, hàng năm thu hút một lượng lớn du khách tới đây lưu trú, nghỉ dưỡng.</p>
+                <p className="object">+</p>
+                <p className="object">5.Tuan Chau Resort Ha Long (La Paz Tuan Chau)</p>
+                <p className="object">-Địa chỉ: Đường Ngọc Châu, đảo Tuần Châu, Thành phố Hạ Long</p>
+                <p className="object">-Giá phòng tham khảo: từ 900.000Đ/phòng/đêm</p>
+                <p className="object">Tuan Chau Resort Ha Long sở hữu quy mô rộng lớn lên đến 220 ha bao gồm 247 phòng nghỉ và biệt thự sang trọng. Các căn biệt thự được thiết kế biệt lập với nhau, tạo sự riêng tư, thoải mái cho các du khách.</p>
+                <p className="object">+</p>
+                <p className="object">Tuan Chau Resort Ha Long lấy cảm hứng thiết kế từ các công trình địa phương của miền Bắc nước Pháp kết hợp cùng với những nét truyền thống của phong cách Việt. Các căn biệt thự ở đây nổi bật với thiết kế đối xứng đặc trưng của các công trình Châu Âu. Thiết kế này không chỉ tạo được tính đồ sộ cho công trình mà đâu đó vẫn mang vẻ đẹp tinh tế, lãng mạn của chất Pháp. </p>
+                <p className="object">+</p>
+                <p className="object">Resort được bao quanh bởi số lượng cây xanh khổng lồ của rừng thông, của những rặng dừa, rặng cọ, những bụi hoa giấy,...Tất cả tạo nên một không gian sống trong lành, mát mẻ và đẹp đẽ nhất.</p>
+                <p className="object">+</p>
+                <p className="object">Điểm nhấn ở phần không gian ngoài trời tại Tuan Chau Resort Ha Long chính là thiết kế bể bơi ngoài trời rộng lớn được đặt ngay trước khu vực sảnh lễ tân. Bể bơi có hướng nhìn ra phía biển, tại đây, du khách hoàn toàn có thể vừa ngâm mình trong làn nước mát, vừa thưởng thức cảnh đẹp thiên nhiên hùng vĩ một cách chân thực nhất.</p>
+                <p className="object">+</p>
+                <p className="object">Tuan Chau Resort Ha Long cung cấp hơn 200 phòng ở với 3 loại biệt thự chính. Các phòng tại Tuan Chau Resort Ha Long đều được thiết kế rộng rãi với vẻ đẹp sang trọng, gồm đầy đủ các tiện ích</p>
+                <p className="object">+</p>
+                <p className="object">Các nhà hàng được thiết kế khác nhau nhưng tổng thể vẫn mang vẻ sang trọng, tinh tế của lối kiến trúc Pháp. Nội thất nhà hàng sử dụng nhiều các vật liệu bằng gỗ từ bàn ghế, sàn nhà, hệ thống cửa…tạo vẻ đẹp giản dị, gần gũi. Bên cạnh đó, ánh sáng vàng được dùng để chiếu sáng phần không gian nhà hàng còn đem đến cho du khách không gian ẩm thực ấm cúng, nhẹ nhàng trong mỗi bữa ăn.</p>
+                <p className="object">+</p>
+            </div>
+        </div>
+
+
+    );
+
+}
+export default Wed;
