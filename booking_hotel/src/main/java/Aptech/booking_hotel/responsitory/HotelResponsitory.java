e1b4b63d428160bef9d70c5e1ccfbd53675cdf6f
dung <dung@192.168.2.2>
2024-05-03 23:50:52 +0700

    3-5

--- booking_hotel/src/main/java/Aptech/booking_hotel/responsitory/HotelResponsitory.java
+++ booking_hotel/src/main/java/Aptech/booking_hotel/responsitory/HotelResponsitory.java
@@ -23,43 +23,6 @@ public interface HotelResponsitory extends JpaRepository<Hotel,Long> {
     @Query("Select h from Hotel h where h.address.city = :city")
     List<Hotel> findHotelsByCity(@Param("city") String city);
 
-    // @Query(
-    //     "select h "+
-    //     "from Hotel h " +
-    //     "join h.rooms r " +
-    //     "left join Availability a On a.room.id = r.id " +
-    //     "and a.date >= :checkinDate AND a.date < :checkoutDate " +
-    //     "where h.address.city = :city " +
-    //     "and (a IS NULL OR a.availableRooms > 0)" +
-    //     "group by h.id, r.id "+
-    //     "having COUNT(DISTINT a.date) + SUM(CASE WHEN a IS NULL THEN 1 ELSE 0 END) = :numberOfDays"
-    //     )
-    //     List<Hotel> findHotelsWithAvailableRooms(@Param("city") String city,
-    //                                              @Param("checkinDate") LocalDate checkinDate,
-    //                                              @Param("checkoutDate") LocalDate checkoutDate,
-    //                                              @Param("numberOfDays") Long numberOfDays);
-
-    // @Query(
-    //     "select h " +
-    //     "where h.address.city = :city " +
-    //     "AND NOT EXIST " +
-    //     "  ( select 1 from Availability a where a.room.hotel.id = h.id and a.date >= :checkinDate AND a.date < :checkoutDate )")
-    //     List<Hotel> findHotelsWithoutAvailabilityRecords(@Param("city") String city,
-    //                                                     @Param("checkinDate") LocalDate checkinDate,
-    //                                                     @Param("checkoutDate") LocalDate checkoutDate,
-    //                                                     @Param("numberOfDays") Long numberOfDays);
-    // @Query(
-    //     "SELECT h FROM Hotel h JOIN h.room r LEFT JOIN Availability a ON r.id = a.room.id " +
-    //     "AND a.date >= :checkinDate AND a.date < :checkoutDate " +
-    //     "WHERE h.address.city = :city " +
-    //     "AND (a IS NULL OR a.availableRooms > 0) "+
-    //     "GROUP BY h.id" + 
-    //     "HAVING COUNT(DISTINCT CASE WHEN a.availableRooms > 0 THEM a.date END) > 0")
-    //     List<Hotel> findHotelsWithPartialAvailabilityRecords(@Param("city") String city,
-    //                                                         @Param("checkinDate") LocalDate checkinDate,
-    //                                                         @Param("checkoutDate") LocalDate checkoutDate,
-    //                                                         @Param("numberOfDays") Long numberOfDays);
-
 
     @Query("SELECT h " +
     "FROM Hotel h " +
