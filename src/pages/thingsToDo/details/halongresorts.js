193ead201273e56cbcddbc156bf67f0593a0a01d
Nampl0 <124085711+Ch0ngLi@users.noreply.github.com>
2024-05-04 17:42:28 +0700

    -

--- src/pages/thingsToDo/details/halongresorts.js
+++ src/pages/thingsToDo/details/halongresorts.js
@@ -17,9 +17,6 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
     return(
         <div className="containerr py-20">
             <h1>I. FLC Grand Hạ Long</h1>
-            <img className="img-article" src={resort01}></img>
-            <p>FLC Grand Hạ Long, các tòa căn hộ khách sạn đẳng cấp 5 sao tọa lạc trên đỉnh đồi trung tâm thành phố. Đây là nơi đẹp nhất để chiêm ngưỡng trọn vẹn kỳ quan thiên nhiên Vịnh Hạ Long. Sở hữu những tiện ích đẳng cấp hàng đầu như sân golf 18 hố dạng links, trung tâm hội nghị quốc tế 1500 chỗ, bể bơi bốn mùa 1000m2 và Skybar view vịnh cao nhất thành phố.</p>
-            <p><b>Địa chỉ</b>: Đường Nguyễn Văn Cừ, phường Hồng Hải, Thành phố Hạ Long</p>
             <p><b>Địa chỉ</b>: Đường Nguyễn Văn Cừ, phường Hồng Hải, Thành phố Hạ Long</p>
             <img className="img-article" src={resort01}></img>
             <p>FLC Grand Hạ Long, các tòa căn hộ khách sạn đẳng cấp 5 sao tọa lạc trên đỉnh đồi trung tâm thành phố. Đây là nơi đẹp nhất để chiêm ngưỡng trọn vẹn kỳ quan thiên nhiên Vịnh Hạ Long. Sở hữu những tiện ích đẳng cấp hàng đầu như sân golf 18 hố dạng links, trung tâm hội nghị quốc tế 1500 chỗ, bể bơi bốn mùa 1000m2 và Skybar view vịnh cao nhất thành phố.</p>
@@ -32,9 +29,6 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <p>Bể bơi bốn mùa trong nhà mang đến không gian bơi lội luôn mát mẻ về mùa hè, ấm nóng về mùa đông. Bạn hoàn toàn yên tâm vì nước được bổ sung, thay mới hàng ngày và không sử dụng hóa chất gây hại làm trong nước nên có thể thỏa thích ngâm mình và bơi lội nhiều giờ liên tục mà không sợ bị hư hại da, tóc.</p>
             <br/>
             <h1>II. Vinpearl Hạ Long</h1>
-            <img className="img-article" src={resort02}></img>
-            <p>Lấy cảm hứng từ các tác phẩm nghệ thuật và kiến ​​trúc Pháp, Vinpearl Hạ Long Resort mang đến cho du khách nơi nghỉ dưỡng sang trọng với thiết kế hình cánh cung trang nhã. Nhìn từ xa, khu nghỉ dưỡng như một vương quốc thu nhỏ, nguy nga và xinh đẹp sừng sững giữa trung tâm Vịnh Hạ Long – kỳ quan thiên nhiên thế giới.</p>
-            <p><b>Địa chỉ</b>: Đảo Rều, Thành phố Hạ Long, Quảng Ninh</p>
             <p><b>Địa chỉ</b>: Đảo Rều, Thành phố Hạ Long, Quảng Ninh</p>
             <img className="img-article" src={resort02}></img>
             <p>Lấy cảm hứng từ các tác phẩm nghệ thuật và kiến ​​trúc Pháp, Vinpearl Hạ Long Resort mang đến cho du khách nơi nghỉ dưỡng sang trọng với thiết kế hình cánh cung trang nhã. Nhìn từ xa, khu nghỉ dưỡng như một vương quốc thu nhỏ, nguy nga và xinh đẹp sừng sững giữa trung tâm Vịnh Hạ Long – kỳ quan thiên nhiên thế giới.</p>
@@ -43,9 +37,6 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <p>Vinpearl Hạ Long không chỉ phù hợp để nghỉ dưỡng, nơi đây còn là địa điểm lý tưởng để bạn có thể trải nghiệm những tiện ích dịch vụ cao cấp, chuẩn 5 sao quốc tế.</p>
             <br/>
             <h1>III. Premier Village Ha Long Bay Resort</h1>
-            <img className="img-article" src={resort03}></img>
-            <p>Với không gian và thiết kế đón gió biển trong lành cùng với những tiện ích như hồ bơi riêng, biệt thự nghỉ dưỡng sang trọng sẽ đem đến cho bạn và gia đình cảm giác được hòa mình vào sự tươi mát của cỏ cây, hoa lá và đắm mình thư giãn trong tiếng sóng biển rì rào.</p>
-            <p><b>Địa chỉ</b>: số 9 đường Hạ Long, Thành phố Hạ Long</p>
             <p><b>Địa chỉ</b>: 9 đường Hạ Long, Thành phố Hạ Long</p>
             <img className="img-article" src={resort03}></img>
             <p>Với không gian và thiết kế đón gió biển trong lành cùng với những tiện ích như hồ bơi riêng, biệt thự nghỉ dưỡng sang trọng sẽ đem đến cho bạn và gia đình cảm giác được hòa mình vào sự tươi mát của cỏ cây, hoa lá và đắm mình thư giãn trong tiếng sóng biển rì rào.</p>
