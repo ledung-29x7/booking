bfe56b53d10d51a73172a3caf64d875c939a7edd
Nampl0 <124085711+Ch0ngLi@users.noreply.github.com>
2024-05-04 19:48:55 +0700

    -

--- src/router/route.js
+++ src/router/route.js
@@ -17,9 +17,23 @@ const publicRoute = [
     { path: '/admin/listUser', componet: ListUser, layout: DefaultLayoutAdmin },
     { path: '/admin/listHotel', componet: ListHotel, layout: DefaultLayoutAdmin },
     { path: '/admin/listBooking', componet: ListBooking, layout: DefaultLayoutAdmin },
+<<<<<<< Updated upstream
     { path: '/admin/listBooking', componet: ListBooking, layout: DefaultLayoutAdmin },
     {path: '/hotel/room', componet: Room },
     {path: '/thingsToDo/halong', componet: HaLongresorts}
+=======
+    { path: '/hotel/room', componet: Room },
+    { path: '/manage/listRooms', componet: ListRoom, layout: DefaultLayoutAdmin },
+    { path: '/manage', componet: HomeManage, layout: DefaultLayoutAdmin },
+    { path: '/manage/myHotels', componet: MyHotel, layout: DefaultLayoutAdmin },
+    { path: '/manage/manageBookings', componet: ManagerBooking, layout: DefaultLayoutAdmin },
+    { path: '/pay', componet: Pay, layout: DefaultLayoutAdmin },
+    { path: '/bookingConfirmation', componet: BookingComfirmation, layout: DefaultLayoutAdmin },
+    { path: '/bookings', componet: MyBookings },
+    { path: '/user/signin', componet: Login, layout: DefaultLayoutLogin },
+    { path: '/user/signup', componet: SignUp, layout: DefaultLayoutLogin },
+    { path: '/thingsToDo/halong', componet: HaLongresorts}
+>>>>>>> Stashed changes
 ]
 
 export default publicRoute;
