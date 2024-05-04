87607bfc097d2e2db281d6d2fa73e85adf7d09f9
Nampl0 <124085711+Ch0ngLi@users.noreply.github.com>
2024-05-04 17:34:10 +0700

    -

--- src/pages/thingsToDo/details/halongresorts.js
+++ src/pages/thingsToDo/details/halongresorts.js
@@ -11,6 +11,7 @@ import resort03b from "../../../image/resorts/resort03b.jpg";
 import resort03c from "../../../image/resorts/resort03c.jpg";
 import resort03d from "../../../image/resorts/resort03d.jpg";
 import resort03e from "../../../image/resorts/resort03e.jpg";
+import resort04 from "../../../image/resorts/resort04.jpg";
 
 function HaLongresorts({src,titleBlog,info,timeUp}){
     return(
@@ -19,6 +20,9 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <img className="img-article" src={resort01}></img>
             <p>FLC Grand Hạ Long, các tòa căn hộ khách sạn đẳng cấp 5 sao tọa lạc trên đỉnh đồi trung tâm thành phố. Đây là nơi đẹp nhất để chiêm ngưỡng trọn vẹn kỳ quan thiên nhiên Vịnh Hạ Long. Sở hữu những tiện ích đẳng cấp hàng đầu như sân golf 18 hố dạng links, trung tâm hội nghị quốc tế 1500 chỗ, bể bơi bốn mùa 1000m2 và Skybar view vịnh cao nhất thành phố.</p>
             <p><b>Địa chỉ</b>: Đường Nguyễn Văn Cừ, phường Hồng Hải, Thành phố Hạ Long</p>
+            <p><b>Địa chỉ</b>: Đường Nguyễn Văn Cừ, phường Hồng Hải, Thành phố Hạ Long</p>
+            <img className="img-article" src={resort01}></img>
+            <p>FLC Grand Hạ Long, các tòa căn hộ khách sạn đẳng cấp 5 sao tọa lạc trên đỉnh đồi trung tâm thành phố. Đây là nơi đẹp nhất để chiêm ngưỡng trọn vẹn kỳ quan thiên nhiên Vịnh Hạ Long. Sở hữu những tiện ích đẳng cấp hàng đầu như sân golf 18 hố dạng links, trung tâm hội nghị quốc tế 1500 chỗ, bể bơi bốn mùa 1000m2 và Skybar view vịnh cao nhất thành phố.</p>
             <img className="img-article" src={resort01a}></img>
             <img className="img-article" src={resort01b}></img>
             <p>FLC Grand Hotel Hạ Long là khách sạn 5 sao duy nhất tại Quảng Ninh sở hữu phòng tổng thống với tiêu chuẩn an ninh hàng đầu, sảnh tiếp đón biệt lập, đầy đủ tiện nghi và dịch vụ.</p>
@@ -31,6 +35,9 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <img className="img-article" src={resort02}></img>
             <p>Lấy cảm hứng từ các tác phẩm nghệ thuật và kiến ​​trúc Pháp, Vinpearl Hạ Long Resort mang đến cho du khách nơi nghỉ dưỡng sang trọng với thiết kế hình cánh cung trang nhã. Nhìn từ xa, khu nghỉ dưỡng như một vương quốc thu nhỏ, nguy nga và xinh đẹp sừng sững giữa trung tâm Vịnh Hạ Long – kỳ quan thiên nhiên thế giới.</p>
             <p><b>Địa chỉ</b>: Đảo Rều, Thành phố Hạ Long, Quảng Ninh</p>
+            <p><b>Địa chỉ</b>: Đảo Rều, Thành phố Hạ Long, Quảng Ninh</p>
+            <img className="img-article" src={resort02}></img>
+            <p>Lấy cảm hứng từ các tác phẩm nghệ thuật và kiến ​​trúc Pháp, Vinpearl Hạ Long Resort mang đến cho du khách nơi nghỉ dưỡng sang trọng với thiết kế hình cánh cung trang nhã. Nhìn từ xa, khu nghỉ dưỡng như một vương quốc thu nhỏ, nguy nga và xinh đẹp sừng sững giữa trung tâm Vịnh Hạ Long – kỳ quan thiên nhiên thế giới.</p>
             <img className="img-article" src={resort02a}></img>
             <p>Với diện tích lên đến 47 000m2, khu nghỉ dưỡng có đến 384 phòng nghỉ với nhiều hạng phòng khác nhau, phù hợp cho nhiều nhu cầu và nhiều đối tượng.</p>
             <p>Vinpearl Hạ Long không chỉ phù hợp để nghỉ dưỡng, nơi đây còn là địa điểm lý tưởng để bạn có thể trải nghiệm những tiện ích dịch vụ cao cấp, chuẩn 5 sao quốc tế.</p>
@@ -39,6 +46,9 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <img className="img-article" src={resort03}></img>
             <p>Với không gian và thiết kế đón gió biển trong lành cùng với những tiện ích như hồ bơi riêng, biệt thự nghỉ dưỡng sang trọng sẽ đem đến cho bạn và gia đình cảm giác được hòa mình vào sự tươi mát của cỏ cây, hoa lá và đắm mình thư giãn trong tiếng sóng biển rì rào.</p>
             <p><b>Địa chỉ</b>: số 9 đường Hạ Long, Thành phố Hạ Long</p>
+            <p><b>Địa chỉ</b>: 9 đường Hạ Long, Thành phố Hạ Long</p>
+            <img className="img-article" src={resort03}></img>
+            <p>Với không gian và thiết kế đón gió biển trong lành cùng với những tiện ích như hồ bơi riêng, biệt thự nghỉ dưỡng sang trọng sẽ đem đến cho bạn và gia đình cảm giác được hòa mình vào sự tươi mát của cỏ cây, hoa lá và đắm mình thư giãn trong tiếng sóng biển rì rào.</p>
             <img className="img-article" src={resort03a}></img>
             <img className="img-article" src={resort03b}></img>
             <p>Những căn biệt thự sang trọng của Premier Village Ha Long Bay được thiết kế tràn đầy cảm hứng để hòa nhịp trọn vẹn với vẻ đẹp nhiệt đới bên bờ vịnh.</p>
@@ -47,6 +57,31 @@ function HaLongresorts({src,titleBlog,info,timeUp}){
             <p>Trải nghiệm những màn trình diễn ẩm thực sống động và tương tác trực tiếp với các đầu bếp hàng đầu.</p>
             <img className="img-article" src={resort03e}></img>
             <p>Cung cấp phòng các phòng massage trị liệu đôi và đơn. Đặc quyền sử dụng bể bơi và spa với trang thiết bị tiện nghi.</p>
+            <br/>
+            <h1>IV. Royal Lotus Ha Long Resort & Villas</h1>
+            <p><b>Địa chỉ</b>: Bán Đảo 2, Đường Hùng Thắng, Thành phố Hạ Long</p>
+            <img className="img-article" src={resort04}></img>
+            <p>Phong cách thiết kế của Resort Royal Lotus Hạ Long được lấy cảm hứng từ xu hướng sống xanh toàn cầu. Công trình là sự kết hợp độc đáo và hoàn hảo giữa vẻ đẹp hiện đại của những khu biệt thự, phòng nghỉ và sự trong lành, tươi mát của phong cảnh thiên nhiên xung quanh.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Royal Villas Hạ Long là khu nghỉ dưỡng duy nhất sở hữu hệ thống phòng khách sạn vừa có phòng nghỉ dưỡng dành cho gia đình. Chính vì vậy resort luôn là sự lựa chọn hàng đầu dành cho những chuyến du lịch nghỉ dưỡng của các đại gia đình hoặc những nhóm du khách đông người.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Không chỉ dừng lại ở đó, nằm trên mảnh đất nhô ra sát bờ biển nên Royal Lotus Hạ Long sở hữu 3 mặt tiền giáp biển. Chính ưu điểm này đã giúp đem tới cảm giác thoáng đãng, bình yên nhưng cũng vô cùng riêng tư cho du khách khi đặt chân tới mảnh đất này.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Với dịch vụ chuyên nghiệp cùng khung cảnh thiên nhiên hữu tình, nên thơ của Hạ Long, Lotus Hạ Long đang là resort được đánh giá cao, hàng năm thu hút một lượng lớn du khách tới đây lưu trú, nghỉ dưỡng.</p>
+            <br/>
+            <h1>Tuan Chau Resort Ha Long (La Paz Tuan Chau)</h1>
+            <p><b>Địa chỉ</b>: Đường Ngọc Châu, đảo Tuần Châu, Thành phố Hạ Long</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Tuan Chau Resort Ha Long sở hữu quy mô rộng lớn lên đến 220 ha bao gồm 247 phòng nghỉ và biệt thự sang trọng. Các căn biệt thự được thiết kế biệt lập với nhau, tạo sự riêng tư, thoải mái cho các du khách.</p>
+            <p>Tuan Chau Resort Ha Long lấy cảm hứng thiết kế từ các công trình địa phương của miền Bắc nước Pháp kết hợp cùng với những nét truyền thống của phong cách Việt. Các căn biệt thự ở đây nổi bật với thiết kế đối xứng đặc trưng của các công trình Châu Âu. Thiết kế này không chỉ tạo được tính đồ sộ cho công trình mà đâu đó vẫn mang vẻ đẹp tinh tế, lãng mạn của chất Pháp.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Resort được bao quanh bởi số lượng cây xanh khổng lồ của rừng thông, của những rặng dừa, rặng cọ, những bụi hoa giấy,...Tất cả tạo nên một không gian sống trong lành, mát mẻ và đẹp đẽ nhất.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Điểm nhấn ở phần không gian ngoài trời tại Tuan Chau Resort Ha Long chính là thiết kế bể bơi ngoài trời rộng lớn được đặt ngay trước khu vực sảnh lễ tân. Bể bơi có hướng nhìn ra phía biển, tại đây, du khách hoàn toàn có thể vừa ngâm mình trong làn nước mát, vừa thưởng thức cảnh đẹp thiên nhiên hùng vĩ một cách chân thực nhất.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Tuan Chau Resort Ha Long cung cấp hơn 200 phòng ở với 3 loại biệt thự chính. Các phòng tại Tuan Chau Resort Ha Long đều được thiết kế rộng rãi với vẻ đẹp sang trọng, gồm đầy đủ các tiện ích.</p>
+            <img className="img-article" src={resort03e}></img>
+            <p>Các nhà hàng được thiết kế khác nhau nhưng tổng thể vẫn mang vẻ sang trọng, tinh tế của lối kiến trúc Pháp. Nội thất nhà hàng sử dụng nhiều các vật liệu bằng gỗ từ bàn ghế, sàn nhà, hệ thống cửa…tạo vẻ đẹp giản dị, gần gũi. Bên cạnh đó, ánh sáng vàng được dùng để chiếu sáng phần không gian nhà hàng còn đem đến cho du khách không gian ẩm thực ấm cúng, nhẹ nhàng trong mỗi bữa ăn.</p>
         </div>
     );
 }
