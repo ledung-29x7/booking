208e2c1401f1a6cfafc7b98093aaf6a95c982fda
dung <dung@192.168.2.2>
2024-05-11 00:23:46 +0700

    11

--- booking_hotel/src/main/java/Aptech/booking_hotel/controller/AuthenticationController.java
+++ booking_hotel/src/main/java/Aptech/booking_hotel/controller/AuthenticationController.java
@@ -4,17 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import Aptech.booking_hotel.model.LoginResponse;
+import Aptech.booking_hotel.model.Role;
+import Aptech.booking_hotel.model.User;
+import Aptech.booking_hotel.model.enums.RoleType;
 import Aptech.booking_hotel.model.validate.LoginDTO;
+import Aptech.booking_hotel.model.validate.UserRegistrationDTO;
+import Aptech.booking_hotel.responsitory.RoleResponsitory;
+import Aptech.booking_hotel.responsitory.UserResponsitory;
 import Aptech.booking_hotel.service.AuthenticationService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
-    @Autowired
+    
+    private UserResponsitory userResponsitory;
+    private RoleResponsitory roleResponsitory;
+    private PasswordEncoder passwordEncoder;
     private AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(
+                            UserResponsitory userResponsitory,
+                            RoleResponsitory roleResponsitory,
+                            PasswordEncoder passwordEncoder,
+                            AuthenticationService authenticationService){
+        this.authenticationService=authenticationService;
+        this.userResponsitory=userResponsitory;
+        this.roleResponsitory=roleResponsitory;
+        this.passwordEncoder=passwordEncoder;
+    }
+
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
@@ -24,5 +47,24 @@ public class AuthenticationController {
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser (@RequestBody UserRegistrationDTO userRegistrationDTO){
+        // kiểm tra xem username đã tồn tại hay chưa
+        if (userResponsitory.existsByUsername(userRegistrationDTO.getUsername())) {
+            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+        }
+        //đăng ký user mới
+        User user = new User();
+        user.setUsername(userRegistrationDTO.getUsername());
+        user.setFirstName(userRegistrationDTO.getFirstName());
+        user.setLastName(userRegistrationDTO.getLastName());
+        user.setPhone(userRegistrationDTO.getPhone());
+        user.setPassword(passwordEncoder.encode(userRegistrationDTO.getPassword())); // mã hòa mật khẩu
+        Role roles = roleResponsitory.findByRoleType(RoleType.CUSTOMER);
+        user.setRole(roles);
+        userResponsitory.save(user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 }
